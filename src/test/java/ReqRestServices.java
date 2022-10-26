353923497b829bcd5ddd4806a92a67ee7d8b477c
marlon-nieves <marlon.3801711612@ucaldas.edu.co>
2022-10-25 20:26:53 -0500

    fix: change method

--- src/test/java/ReqRestServices.java
+++ src/test/java/ReqRestServices.java
@@ -1,8 +1,9 @@
 import org.junit.jupiter.api.Test;
 
 public class ReqRestServices {
+    String date = "27/05/1999";
     @Test
-    public void helloTestingWorld(){
-        System.out.printf("Hello Testing World");
+    public void birthdayDate(){
+        System.out.printf("Mi fecha de nacimiento es: "+ date);
     }
 }
