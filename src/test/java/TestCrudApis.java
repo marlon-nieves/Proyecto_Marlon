db38d1a046d46abef5ccc74c5bc79a5f7f633cb1
davalenciar <ldsalejo@gmail.com>
2022-11-03 15:28:59 -0500

    Commit Reto 1

--- src/test/java/TestCrudApis.java
+++ src/test/java/TestCrudApis.java
@@ -1,6 +1,13 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class TestCrudApis {
     @BeforeEach
@@ -11,7 +18,7 @@ public class TestCrudApis {
         RestAssured.requestSpecification = new RequestSpecBuilder().setContentType(ContentType.JSON).build();
     }
     @Test
-    public void TestPostLogin(){
+    public void PostLoginUnknown(){
         given()
                 .body("{\n" +
                         "    \"email\": \"eve.holt@reqres.in\",\n" +
@@ -21,37 +28,36 @@ public class TestCrudApis {
                 .then().statusCode(HttpStatus.SC_OK);
     }
     @Test
-    public void TestGet(){
+    public void GetListUnknown(){
         RestAssured.when().get("https://reqres.in/api/unknown").
                 then()
-                .body("page",equalTo(1));
+                .body("page",equalTo(1))
+                .body("token",notNullValue());
     }
     @Test
-    public void TestPutUpdate(){
+    public void PutUpdateUnknown(){
         given()
                 .log().all()
                 .body("{\n" +
-                        "    \"name\": \"Wilber Perez\",\n" +
+                        "    \"name\": \"Diego Valencia\",\n" +
                         "    \"job\": \"QA Automation\"\n" +
                         "}")
                 .put("https://reqres.in/api/users/7");
     }
     @Test
-    public void TestDelete(){
+    public void DeleteUnknown(){
         RestAssured.when().delete("https://reqres.in/api/users/2");
     }
-    @Test
-    public void patch(){
-        String nameUpdate = given().
-                when()
-                .body("{\n" +
-                        "    \"name\": \"Andres\",\n" +
-                        "    \"job\": \"zion resident\"\n" +
-                        "}")
-                .patch("user/2").then().statusCode(HttpStatus.SC_OK)
-                .extract().jsonPath().get("name");
-        assertThat(nameUpdate,equalTo("Andres"));
-
-    }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
